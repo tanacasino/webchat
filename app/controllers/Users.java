9c8cab1da70eda17278983944662fc9261b2a0b6
tanacasino <tanacasino@gmail.com>
2011-10-28 00:40:07 +0900

    Add Avatar to chat message.

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -1,7 +1,9 @@
 package controllers;
 
-import play.mvc.With;
 import models.User;
+import play.Play;
+import play.mvc.With;
+import play.vfs.VirtualFile;
 
 @With(Security.class)
 public class Users extends CRUD {
@@ -9,6 +11,13 @@ public class Users extends CRUD {
     public static void avatar(String username) {
         final User user = User.findByName(username);
         notFoundIfNull(user);
+        if (!user.avatar.exists()) {
+            VirtualFile vf = Play.getVirtualFile("/public/images/icons/user.png");
+            if (vf == null || !vf.exists()) {
+                notFound();
+            }
+            renderBinary(vf.getRealFile());
+        }
         response.setContentTypeIfNotSet(user.avatar.type());
         renderBinary(user.avatar.get());
     }
